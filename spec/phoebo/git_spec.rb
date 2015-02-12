fb26c2813102936abef9519733cf0759af28678a
Adam Staněk <adam.stanek@v3net.cz>
2015-02-12 18:24:13 +0100

    Added test for Git

---
+++ spec/phoebo/git_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../spec_helper'
+
+describe Phoebo::Git do
+
+  subject { described_class }
+
+  context 'without credentials' do
+    let(:request) do
+      Phoebo::Request.new do
+        repo_url = 'ssh://somehost.tld/user/repo.git'
+        ssh_private_file = 'key'
+        ssh_public_file = 'key.pub'
+      end
+    end
+
+    it 'clones' do
+      expect(Rugged::Repository).to receive(:clone_at)
+      subject.clone(request, '/tmp/somepath')
+    end
+  end
+
+  context 'with SSH credentials' do
+    let(:request) do
+      Phoebo::Request.new do
+        repo_url = 'ssh://somehost.tld/user/repo.git'
+      end
+    end
+
+    it 'clones' do
+      expect(Rugged::Repository).to receive(:clone_at)
+      subject.clone(request, '/tmp/somepath')
+    end
+  end
+
+end
